f683fdaa49602b62db1cea430a7683e73732434e
Pedro Henrique Gardim Miranda <me@pedrogardim.com>
2023-03-21 16:25:39 +0100

    Little sun

--- Assets/Scrips/GirlController.cs
+++ Assets/Scrips/GirlController.cs
@@ -10,12 +10,14 @@ public class GirlController : MonoBehaviour
     public Rigidbody2D rigidBody;
     public SpriteRenderer spriteRenderer;
     GameController gameController;
+    GameObject littleSun;
 
     void Start()
     {
         gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         rigidBody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        littleSun = gameObject.transform.GetChild(0).gameObject;
         rigidBody.velocity = new Vector3(speed * (direction ? 1 : -1), 0,0);
     }
 
@@ -29,8 +31,14 @@ public class GirlController : MonoBehaviour
         direction = !direction;
         spriteRenderer.flipX = !direction;
         rigidBody.velocity = new Vector3(speed * (direction ? 1 : -1), 0,0);
+
         if(col.gameObject.CompareTag("WalkStart")){
             gameController.addSun(power);
+            littleSun.SetActive(false);
         }
+        else{
+            littleSun.SetActive(true);
+        }
+
     }
 }
